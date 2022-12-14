1440a391f59e2646f96411e24d2c75c8db8b6cbb
Prathamesh Moharkar <prathameshmoharkar125@gmail.com>
2022-12-14 13:50:19 +0530

    IDP Provider Module Completed

---
+++ apps/server/src/server.constants.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const constants = {
+  firebaseConfig: {
+    FIREBASE_TYPE: process.env.FIREBASE_TYPE,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_PRIVATE_KEY_ID: process.env.FIREBASE_PRIVATE_KEY_ID,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_CLIENT_ID: process.env.FIREBASE_CLIENT_ID,
+    FIREBASE_AUTH_URI: process.env.FIREBASE_AUTH_URI,
+    FIREBASE_TOKEN_URI: process.env.FIREBASE_TOKEN_URI,
+    FIREBASE_AUTH_PROVIDER_X509: process.env.FIREBASE_AUTH_PROVIDER_X509,
+    FIREBASE_CLIENT_X509: process.env.FIREBASE_CLIENT_X509,
+  },
+};
